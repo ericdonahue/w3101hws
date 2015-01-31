a0e8ccdf587cbfc47c7a0699dab41504b8f42554
Eric Donahue <emd2163@columbia.edu>
2015-01-31 16:33:28 -0500

    completed broken-add assignment

--- hw1/src/broken-add/brokenAdd.js
+++ hw1/src/broken-add/brokenAdd.js
@@ -2,8 +2,8 @@
   'use strict';
 
   var add = function (a, b) {
-    // ONLY MAKE CHANGES INSIDE THIS FUNCTION
-    a + b; // THIS ISN'T WORKING! CHANGE IT SO THAT IT DOES WORK!
+	var sum = a + b;
+	return sum;
   };
 
   module.exports = add; // DO NOT CHANGE THIS!
